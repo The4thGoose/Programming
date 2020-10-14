c083b3c70f1ae152d07cdfe257fc2976f09479fb
da_flaming_fist <kpwenzel@STU.naperville203.org>
2020-10-14 13:29:42 -0500

    Started PBS Pledge program

---
+++ Chapter 3/WenzelPBSPledgeProgram.py
@@ -0,0 +1,7 @@
+# Kyle Wenzel
+# October 14th, 2020
+# PBS Pledge Program
+# This program will ask for donator's name, how much they pledged, how many people pledged, how much was made, and the average pledge
+
+print("\nWelcome to the PBS Pledge Program! ")
+
