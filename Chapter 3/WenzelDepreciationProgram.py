93824611a66490a5cb2dee988bebe355f50fc9b0
da_flaming_fist <kpwenzel@STU.naperville203.org>
2020-10-19 11:17:50 -0500

    Started Working on Depreciation Program

--- Chapter 3/WenzelDepreciationProgram.py
+++ Chapter 3/WenzelDepreciationProgram.py
@@ -1,4 +1,21 @@
 # Kyle Wenzel
 # October 18th, 2020
 # Depreciation program
-# This program will 
+# This program will calculate depreciation on a machine
+
+
+assetname = input("\nWhat is the asset name? ")
+cost = float(input("What is the cost of", assetname + "? "))
+salvagevalue = float(input("What is the salvage value of the", assetname + "? "))
+usefullife = float(input("What is the useful life of the", assetname + "? "))
+
+depreciation = (cost - salvagevalue) / usefullife
+
+print("Great Ep-Hick Company")
+print("Accounting Department")
+print("")
+print("{:<30}{:<30}".format())
+print("{:<30}{:<30}".format())
+print("{:<30}{:<30}".format())
+print("{:<30}{:<30}".format())
+print("{:<30}{:<30}".format())
