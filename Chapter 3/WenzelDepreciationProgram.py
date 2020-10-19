dfa26e73c6bd92d9db5809c54de39b368e8f8747
da_flaming_fist <kpwenzel@STU.naperville203.org>
2020-10-19 13:31:37 -0500

    finished Depreciation Program

--- Chapter 3/WenzelDepreciationProgram.py
+++ Chapter 3/WenzelDepreciationProgram.py
@@ -3,19 +3,27 @@
 # Depreciation program
 # This program will calculate depreciation on a machine
 
+continuewhile = "y"
 
-assetname = input("\nWhat is the asset name? ")
-cost = float(input("What is the cost of", assetname + "? "))
-salvagevalue = float(input("What is the salvage value of the", assetname + "? "))
-usefullife = float(input("What is the useful life of the", assetname + "? "))
+while continuewhile.lower() == "y":
+    assetname = input("\nWhat is the asset name? ")
+    cost = float(input("What is the cost of " + assetname + "? "))
+    salvagevalue = float(input("What is the salvage value of the " + assetname + "? "))
+    usefullife = float(input("What is the useful life of the " + assetname + "? "))
 
-depreciation = (cost - salvagevalue) / usefullife
+    depreciation = (cost - salvagevalue) / usefullife
+    
+    print("Great Ep-Hick Company")
+    print("Accounting Department")
+    print("")
+    print("{:<30}{:<30}".format("Name:", assetname))
+    print("{:<30}${:<30,.2f}".format("Cost:", cost))
+    print("{:<30}${:<30,.2f}".format("Salvage Value:", salvagevalue))
+    print("{:<30}{:<30}".format("Useful Life:", usefullife))
+    print("{:<30}${:<30,.2f}".format("Depreciation:", depreciation))
 
-print("Great Ep-Hick Company")
-print("Accounting Department")
-print("")
-print("{:<30}{:<30}".format())
-print("{:<30}{:<30}".format())
-print("{:<30}{:<30}".format())
-print("{:<30}{:<30}".format())
-print("{:<30}{:<30}".format())
+    continuewhile = input("\nWould you like to continue? (y/n) ")
+    if continuewhile.lower() == "yes":
+        continuewhile = "y"
+    elif continuewhile == "":
+        exit()
