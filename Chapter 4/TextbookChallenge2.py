e615030297ee0ea344958ddddb80a7603310cce7
Kyle Wenzel <kpwenzel@STU.naperville203.org>
2020-12-04 08:32:32 -0600

    w3schools.com is a savior

---
+++ Chapter 4/TextbookChallenge2.py
@@ -0,0 +1,13 @@
+# Kyle Wenzel
+# MonthDay, 2020
+# ProgramName Program
+
+print("Welcome to the ProgramName Program\n")
+
+message = input("What is your message? ")
+
+message = message[::-1]
+
+print(message)
+
+input("\nPress enter to exit...")
