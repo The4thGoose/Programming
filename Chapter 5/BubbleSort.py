ecd0c0054b231d4c66447b039ea1ea7416010422
Kyle Wenzel <kpwenzel@STU.naperville203.org>
2020-12-08 12:39:18 -0600

    WEEB SHIT

---
+++ Chapter 5/BubbleSort.py
@@ -0,0 +1,8 @@
+numbers = [5, 8, 25, 18]
+
+numbers.append(2)
+numbers.append(12)
+
+numbers.sort()
+
+print(numbers)
---
+++ Chapter 5/WenzelGameProgram.py
@@ -0,0 +1,23 @@
+# Kyle Wenzel
+# December 8th, 2020
+# Game Program
+
+print("Welcome to the Games program. ")
+
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+counter = 1
+
+print ("Welcome to the Candy Program")
+#Step 1:______
+game = []
+numberlist = 1
+for item in range (1,6):
+  FGAME = input("What is game #" + str(numberlist) + "? ")
+  numberlist += 1
+  game.append(FGAME)
+
+game.sort()
+for item in game: 
+  print(item)
