bbfc99770a0789d22cc6b483b22db8c265d7a9b6
Kyle Wenzel <kpwenzel@STU.naperville203.org>
2020-12-22 12:25:59 -0600

    AAAAAAAAAAAH WINTER BREAK

--- Chapter 5/WenzelEmployeeSalary.py
+++ Chapter 5/WenzelEmployeeSalary.py
@@ -5,7 +5,36 @@
 print("Welcome to the ProgramName Program\n")
 
 # Step 1: 
+employeelist = []
+salarylist = []
+ticker = 0
+totalsalary = 0
+above = 0
+atav = 0
+below = 0
+anotheremployee = "y"
+while anotheremployee == "y":
+    employee = input("What is the employee's name? ")
+    employeelist.append(employee)
+    ansalary = float(input("What is the annual salary for this employee? "))
+    salarylist.append(ansalary)
+    ticker += 1
+    anotheremployee = input("Whould you like to enter another employee? (y or n) ")
+for entry in salarylist:
+    totalsalary += entry
+averagesalary = totalsalary / ticker
+for entry in salarylist: 
+    if entry < averagesalary:
+        below += 1
+    elif entry == averagesalary: 
+        atav += 1
+    elif entry > averagesalary: 
+        above += 1
+print("{:}${:,.2f}".format("Total Employee Salary:", totalsalary))
+print("{:}${:,.2f}".format("Average Employee Salary:", averagesalary))
 
-
+print("{:}{:,}".format("Number of Employees Making More than the Average:", above))
+print("{:}{:,}".format("Number of Employees Making the Average:", atav))
+print("{:}{:,}".format("Number of Employees Making Less than the Average:", below))
 # Step : Wait for escape keystroke
 input("\nPress enter to exit...")
