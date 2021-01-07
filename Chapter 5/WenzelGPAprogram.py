2b6a6e839166bce11e807fbe55109dd7edbacbb5
Kyle Wenzel <kpwenzel@STU.naperville203.org>
2021-01-07 14:42:10 -0600

    HAHAHA SANS GO BRRRRRRRRRRRRRRRRR

---
+++ Chapter 5/WenzelGPAprogram.py
@@ -0,0 +1,10 @@
+# Kyle Wenzel
+# January 7th, 2021
+# GPA Program
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+print("Welcome to the GPA program! ")
+
+Students= [()]
+
+input("Press enter to exit...")
